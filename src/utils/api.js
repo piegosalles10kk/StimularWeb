128d7b062b9ab11ec2074e935df08d3b5063614b
Diego Salles <104213637+piegosalles10kk@users.noreply.github.com>
2025-01-27 15:09:06 -0300

    <troca-API>

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,4 @@
 //export const api = "http://localhost:3001";
 
-export const api = "https://stimularweb.shop";
+export const api = "https://stimularbackend.fly.dev";
+export const api1 = "https://stimularweb.shop"
