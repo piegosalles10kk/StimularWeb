00996bcfb6961d1e8735072b1c6bc8018f64b211
Diego Salles <104213637+piegosalles10kk@users.noreply.github.com>
2025-04-08 10:46:18 -0300

    update-finalizado

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-export const api1 = "https://stimularbackend.fly.dev";
-export const api = "http://localhost:3001";
+export const api = "https://stimularbackend.fly.dev";
+export const api1 = "http://localhost:3001";
